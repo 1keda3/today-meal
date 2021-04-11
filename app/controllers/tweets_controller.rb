b021145c3551bfb8fc1ea2bd5059bba99eb9c600
1keda3 <xrzrutosa@yahoo.co.jp>
2021-04-11 21:51:03 +0900

    編集、削除機能

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -14,10 +14,34 @@ class TweetsController < ApplicationController
     @tweet = Tweet.find(params[:id])
   end
 
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def update
+    tweet = Tweet.find(params[:id])
+    if tweet.update(update_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+
+  def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy
+    redirect_to root_path
+  end
+  
   private
 
   def tweet_params
     params.permit(:title, :eat_time, :time_taken, :recipe, :supplement, :image).merge(user_id: current_user.id)
   end
-  
+
+  def update_params
+    params.require(:tweet).permit(:title, :eat_time, :time_taken, :recipe, :supplement, :image).merge(user_id: current_user.id)
+  end
+
 end
